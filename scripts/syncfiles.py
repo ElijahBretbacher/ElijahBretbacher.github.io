c1ec352780b08ca2ec700ee529a035d5021b93a7
Elijah <bretbacher.elijah@gmail.com>
2024-11-03 18:08:06 +0100

    aaaaa

---
+++ scripts/syncfiles.py
@@ -0,0 +1,32 @@
+import os
+from google.oauth2 import service_account
+from googleapiclient.discovery import build
+
+# Set up Google Drive API
+SCOPES = ['https://www.googleapis.com/auth/drive.readonly']
+SERVICE_ACCOUNT_FILE = '_data/assetlibrary_token.json'
+creds = service_account.Credentials.from_service_account_file(
+    SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+drive_service = build('drive', 'v3', credentials=creds)
+
+# Recursively list file names
+def list_files(folder_id, parent_path=''):
+    results = drive_service.files().list(
+        q=f"'{folder_id}' in parents",
+        fields='files(id, name, mimeType)').execute()
+    files = results.get('files', [])
+    for file in files:
+        file_id = file['id']
+        file_name = file['name']
+        if file['mimeType'] == 'application/vnd.google-apps.folder':
+            new_folder_path = os.path.join(parent_path, file_name)
+            print(f"Folder: {new_folder_path}")
+            list_files(file_id, new_folder_path)
+        else:
+            file_path = os.path.join(parent_path, file_name)
+            print(f"File: {file_path}")
+
+if __name__ == '__main__':
+    root_folder_id = '1dR20I3dbz-oK8dFvabYjUfIGnJlEyC-n'  # Replace with your root folder ID
+    list_files(root_folder_id)
+
